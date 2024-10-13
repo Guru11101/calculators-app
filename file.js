e308a67efc772ed007b0cb6471e6fdc39e6f3a4e
Guru11101 <125476838+Guru11101@users.noreply.github.com>
2024-10-13 02:33:36 -0700

    Add files via upload

---
+++ file.js
@@ -0,0 +1,34 @@
+let display=document.getElementById('inputBox');
+let buttons=document.querySelectorAll('button')
+buttonsArray=Array.from(buttons)
+
+let string='';
+
+buttonsArray.forEach(btn => {
+    btn.addEventListener('click', (e)=>{
+
+
+        if(e.target.innerHTML=='DEL'){
+            string=string.substring(0, string.length-1);
+            display.value = string;
+        }else if(e.target.innerHTML=='RESET'){
+                string="";
+                display.value = string;
+        }else if(e.target.innerHTML=='='){
+            string=eval(string);
+            display.value = string;
+    }
+        
+        
+        else{
+            string +=e.target.innerHTML;
+            display.value = string;  
+        }
+
+        
+ 
+
+    
+})
+})
+
